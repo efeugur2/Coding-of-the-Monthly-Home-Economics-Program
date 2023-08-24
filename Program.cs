c8037490431c9e1009c3073c4bf2f6f3e42fb0c1
Efe Uğur <efeugurkurnaz2@gmail.com>
2023-08-24 11:55:26 +0300

    Coding of the monthly home economics program
    Home economics program

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: kurna
+ * Date: 23.08.2023
+ * Time: 13:50
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace proje2
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			int maas = 0 ;
+			int mutfakGideri = 0;
+			int elektrikGideri = 0;
+			int suGideri = 0;
+			int	telefonGideri= 0;
+			int	internet = 0;
+			int giyim = 0;
+			int diğer = 0;
+			
+			Console.WriteLine("Maaşınızı giriniz");
+			string okunanDeğer = Console.ReadLine();
+			maas = Convert.ToInt32(okunanDeğer);
+			Console.WriteLine("Mutfak gideri giriniz");
+			mutfakGideri = Convert.ToInt32(Console.ReadLine());
+		
+			Console.WriteLine("Elektrik gideri giriniz");
+			elektrikGideri = Convert.ToInt32(Console.ReadLine());
+			
+			Console.WriteLine("Su gideri giriniz");
+			suGideri = Convert.ToInt32(Console.ReadLine());
+			
+			Console.WriteLine("Telefon gideri giriniz");
+			telefonGideri = Convert.ToInt32(Console.ReadLine());
+			
+			Console.WriteLine("İnternet gideri giriniz");
+			internet = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Giyim gideri giriniz");
+			giyim = Convert.ToInt32(Console.ReadLine());
+			
+			Console.WriteLine("Diğer gideri giriniz");
+			diğer = Convert.ToInt32(Console.ReadLine());
+			
+			int sonuc = maas - mutfakGideri - elektrikGideri - suGideri - telefonGideri - internet -giyim - diğer;
+			Console.WriteLine("\n Eline kalan miktar: "+ sonuc);
+			Console.ReadLine();
+		}
+	}
+}
